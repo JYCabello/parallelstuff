b75cc35856bc3321662b3c232f021f78e1146aa3
Yeray Cabello <j.cabello.ramos@gmail.com>
2023-03-13 16:01:41 +0100

    ruining the weird calculator

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { createClient, gql } from "@urql/core";
 import 'isomorphic-unfetch';
 
 const client = createClient({
-    url: 'http://localhost:8080/graphql',
+    url: 'https://avya4kkcqc.execute-api.us-east-1.amazonaws.com/production/graphql',
   });
 
 const listInventories = gql`
@@ -49,7 +49,7 @@ async function hitIt() {
         )
     })
 }
-function delay (miliseconds: number) {
+function delay(miliseconds: number) {
     return new Promise<void>((resolve) => {
         setTimeout(() => {
             resolve();
@@ -58,13 +58,56 @@ function delay (miliseconds: number) {
 }
 
 
+async function doStockStuff() {
+  await Promise
+    .allSettled([...Array(1)].map(x => hitIt()))
+    .then(async () => {
+        // await delay(5000)
+        // const r = await client
+        //     .query(listInventories, {})
+        //     .toPromise();
+        // return console.log(r.data.ListInventories.items);
+    });
+}
 
-Promise.allSettled([...Array(15).keys()].map(x => hitIt()))
-.then(async () => {
-    // await delay(5000)
-    // const r = await client
-    //     .query(listInventories, {})
-    //     .toPromise();
-    // return console.log(r.data.ListInventories.items);
-})
+const add10 = gql`
+mutation add10 {
+    Add(input: { additionId: "fee75879-9150-4992-a51e-daca256aa9e6", amount: 1 })
+}
+`;
 
+const getaddition = gql`
+query {
+  ListAdditionReadModels {
+    items {
+      id
+      amount
+    }
+    count
+  }
+}
+`;
+
+async function showAdditions() {
+  const preQueryResult = await client.query(getaddition, {}).toPromise();
+  if (preQueryResult.data?.ListAdditionReadModels.items) {
+    console.log(preQueryResult.data.ListAdditionReadModels.items[0].amount);
+  }
+}
+
+async function addStuff() {
+  await showAdditions();
+  await Promise
+    .allSettled([...Array(100)]
+    .map(async () => {
+      const result = await client.mutation(add10, {}).toPromise();
+      if (!(result.data.Add)) {
+        console.log("One mutation failed");
+      }
+    }));
+  console.log("sent all mutations");
+  await delay(10_000);
+  await showAdditions();
+}
+
+addStuff();
